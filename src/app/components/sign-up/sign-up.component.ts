a2186a89622df40bfbf4fda12ab721efe9ca15aa
Alex <obys@Alecs>
2022-12-27 14:19:30 +0200

    solved buttons

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,7 +8,12 @@ import { AuthService } from "../../shared/services/auth.service";
 })
 export class SignUpComponent implements OnInit {
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    public router: Router
   ) { }
+  // create function to navigate to sign in page with redirect
+  goToSignIn() {
+    this.router.navigate(['/sign-in']);
+  }
   ngOnInit() { }
 }
